7e253b835984aa930afad60deea08fee8a8f3341
Adrien Lina <adrienlina@gmail.com>
2018-02-08 12:20:45 +0100

    Webhook try

---
+++ routes/webhook.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var router = express.Router();
+var config = require('config');
+
+/* GET hello world page. */
+router.get('/', function(req, res, next) {
+  // Your verify token. Should be a random string.
+  // Parse the query params
+  var mode = req.query['hub.mode'];
+  var token = req.query['hub.verify_token'];
+  var challenge = req.query['hub.challenge'];
+
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
+    // Checks the mode and token sent is correct
+    if (mode === 'subscribe' && token === config.validationToken) {
+      // Responds with the challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
+    }
+  }
+});
+
+router.post('/', (req, res) => {
+  // Parse the request body from the POST
+  var body = req.body;
+  // Check the webhook event is from a Page subscription
+  if (body.object === 'page') {
+    // Iterate over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
+      // Get the webhook event. entry.messaging is an array, but
+      // will only ever contain one event, so we get index 0
+      var webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+    });
+    // Return a '200 OK' response to all events
+    res.status(200).send('EVENT_RECEIVED');
+  } else {
+    // Return a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
+});
+
+module.exports = router;
